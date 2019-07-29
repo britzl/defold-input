abbad7ddb33815ed913419231027f13559a8faf7
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-07-29 17:08:57 +0200

    Moved cursor message id constants to a Lua module

---
+++ in/cursor.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.OVER = hash("cursor_over")
+M.OUT = hash("cursor_out")
+M.PRESSED = hash("pressed")
+M.RELEASED = hash("released")
+M.DRAG_END = hash("drag_end")
+M.DRAG_START = hash("drag_start")
+
+return M
