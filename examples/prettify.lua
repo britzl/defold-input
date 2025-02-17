6e8b487ea09483ecc8a9b8ead6e6975077752f01
Björn Ritzl <bjorn.ritzl@gmail.com>
2025-02-17 20:20:18 +0100

    Added option to disable swipe on release only

--- examples/prettify.lua
+++ examples/prettify.lua
@@ -4,11 +4,18 @@ local M = {}
 function M.prettify(t, indentation)
 	indentation = indentation or ""
 	local s = ""
+	local keys = {}
 	for k,v in pairs(t) do
+		keys[#keys + 1] = k
+	end
+	for i=1,#keys do
+		local k = keys[i]
+		local v = t[k]
+		local last = (i == #keys) and "" or ",\n"
 		if type(v) == "table" then
-			s = s .. ("%s%s = {\n%s},\n"):format(indentation, k, M.prettify(v, indentation .. "  "))
+			s = s .. ("%s%s = {\n%s\n}%s"):format(indentation, k, M.prettify(v, indentation .. "  "), last)
 		else
-			s = s .. ("%s%s = %s,\n"):format(indentation, k, tostring(v))
+			s = s .. ("%s%s = %s%s"):format(indentation, k, tostring(v), last)
 		end
 	end
 	return s
